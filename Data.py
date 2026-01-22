bc723d9e35bcf5ac89af42a98dc6ef4fb644e28b
Veer Odedra <81079937+veeroded@users.noreply.github.com>
2026-01-22 14:37:30 +0000

    UI Start Data pretty
    refactoring of data beggining aswell the data requests are to ong a function to make easier and quicker to import data

--- Data.py
+++ Data.py
@@ -4,28 +4,52 @@ import json
 with open("Token.txt", "r") as f:
     Bearer = f.read()
 
+def JsonDump(int Bearer,str item, file)
+    with open(f"./Data/{file}", "w0"):
+        json.dump(
+            json.loads(get(
+                f"https://api.spacetraders.io/v2/{item}",
+                headers={"Authorization": f"Bearer {Bearer}"},
+            ).text),
+            f,
+            indent = 4
+        )
+        
+
 
 def Agent(Bearer):
     f = open("./Data/Agent.json", "w")
     json.dump(
-        get(
+        json.loads(get(
             "https://api.spacetraders.io/v2/my/agent",
             headers={"Authorization": f"Bearer {Bearer}"},
-        ).json(),
+        ).text),
         f,
+        indent = 4
     )
 
 
 def Systems(Bearer):
     f = open("./Data/Systems.json", "w")
     json.dump(
-        get(
+        json.loads(get(
             "https://api.spacetraders.io/v2/systems",
             headers={"Authorization": f"Bearer {Bearer}"},
-        ).json(),
+        ).text),
         f,
+        indent = 4
     )
-
-
-print(Bearer)
-# Systems(Bearer)
+    
+def TradeRelations(Bearer):
+        f = open("./Data/Systems.json", "w")
+    json.dump(
+        json.loads(get(
+            "https://api.spacetraders.io/v2/systems",
+            headers={"Authorization": f"Bearer {Bearer}"},
+        ).text),
+        f,
+        indent = 4
+    )
+    
+Systems(Bearer)
+Agent(Bearer)
